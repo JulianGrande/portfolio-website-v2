e41ab187368ed8e622fdab144d8030fa882d4531
Julian Grande <juliangrande11@gmail.com>
2024-03-03 22:45:16 -0500

    added pic and fixed about section; need to fix timeline and then pics and then android read me

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import placeholder from '../../../public/my_images/Specialty_Pizza.jpg'
+import pic from '../../../public/my_images/mirror_pic_blue_shirt.jpg'
 
 const About = () => {
     return (
@@ -23,13 +24,13 @@ const About = () => {
                     </p>
 
                     <p className="py-2 text-[#222525] text-left font-[Arial]">
-                        Besides Software, I am also deeply passionate about cars and their performance, particularly f1 racing.
-                        In my spare time, I like to do weightlifting
+                        Besides Software, I am also deeply passionate about cars, and
+                        in my spare time, I like to do weightlifting
                         and I love to listen to rnb and pop music. 
                     </p>
                 </div>
                 <div className="w-full h-auto m-auto shadow-s shadow-gray-700 rounded-xl items-center justify-center p-4 hover:scale-75 ease-in duration-200 hidden md:inline-block lg:inline-block">
-                    <Image src={placeholder} className="rounded-xl" alt='/'></Image>
+                    <Image src={pic} className="rounded-xl" alt='/'></Image>
                 </div>
             </div>    
         </div>
